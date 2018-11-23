764b8ef4b21ab5ccf463e2eeba8c2ec806bfd107
xyj <yuanjianxuxx@gmail.com>
2018-11-23 15:05:48 -0600

    almost finished Todo1&2&3

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,24 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        
+        public IActionResult Results(string searchType, string searchTerm)
+         {
+             if (searchType.Equals("all"))
+             {
+                 List<Dictionary<string, string>> Jobs = JobData.FindByValue(searchTerm);
+                 ViewBag.title = "Search In All Jobs";
+                 ViewBag.Jobs = Jobs;
+                 return View();
+             }
+             else
+             {
+                List<Dictionary<string, string>> Jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "Search In"+ searchType + ":";
+                ViewBag.Jobs = Jobs;
+                return View();
+             }
 
+         }
     }
 }
